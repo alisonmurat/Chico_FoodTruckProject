4f3eccfde249e33c87b51ce99f11676fa5ee210e
xnativi05 <xnativi@masonlive.gmu.edu>
2016-03-20 16:26:32 -0400

    AboutRoutesChanges

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,15 +29,6 @@ Route::get('/gallery', function () {
     return view('gallery');
 });
 
-Route::get('/aboutus', function () {
-    return view('about_us');
-});
-
-Route::get('/admin', function () {
-    return view('admin');
-});
-
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
