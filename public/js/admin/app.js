3d91b347cb12cde73d00ac205fe759057b933dac
yfeng4 <yf2723@gmail.com>
2016-03-22 19:26:07 -0400

    Minor change on App.js

--- public/js/admin/app.js
+++ public/js/admin/app.js
@@ -5,8 +5,7 @@ app.config(['$routeProvider',function($routeProvider) {
 		.when('/items/:id?', {
 			controller: 'items/Controller',
 			templateUrl: '/js/admin/partials/itemsTmpl.html'
-			
-			$routeProvider
+		})
 		.when('/about_us/:id?', {
 			controller: 'about_us/Controller',
 			templateUrl: '/js/admin/partials/about_usTmpl.html'
