f22533a8bf769e71d7841e269dd5b7bb45493036
yfeng4 <yf2723@gmail.com>
2016-03-22 21:53:47 -0400

    added about_usService.js

---
+++ public/js/admin/services/about_usService.js
@@ -0,0 +1,27 @@
+app.factory('about_us' function ($http) {
+
+	return {
+
+		// Get all pages from server / DB
+		list: function () {
+			return $http.get('/ajax/about_us');
+		},
+
+		// Get a single page from the server / DB
+		get: function (id) {
+			return $http.get('/ajax/about_us/'+id);
+		},
+
+		// Save a new page to the server / DB
+		save: function (data) {
+			return $http.post('/ajax/about_us', data);
+		},
+
+		// Update a page on the server / DB
+		update: function (id, data) {
+			return $http.put('/ajax/about_us/'+id, data);
+		}
+
+	}
+
+});
