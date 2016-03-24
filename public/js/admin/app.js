ef5f1ada89e850d78d4ba015cb7179b595101925
rchoudh7 <rchoudh7@masonlive.gmu.edu>
2016-03-24 12:38:25 -0400

    routes

--- public/js/admin/app.js
+++ public/js/admin/app.js
@@ -3,11 +3,11 @@ var app = angular.module('adminApp', ['ngRoute']);
 app.config(['$routeProvider',function($routeProvider) {
 	$routeProvider
 		.when('/items/:id?', {
-			controller: '/js/admin/app',
+			controller: '/js/admin/controllers/ItemsController',
 			templateUrl: '/js/admin/partials/itemsTmpl.html'
 		})
 		.when('/about_us/:id?', {
-			controller: 'about_us/Controller',
+			controller: '/js/admin/controllers/about_usController',
 			templateUrl: '/js/admin/partials/about_usTmpl.html'
 		})
 		
