c730df0c9e026cb62effa8ec62830c781db1e143
alisonmurat <amurat@gmu.edu>
2016-03-24 12:17:32 -0400

    Gallery Table

---
+++ database/migrations/2016_03_20_193737_create_items_table copy.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(‘gallery’, function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string(‘Image’);
+            $table->string(‘GalleryDescription');
+            $table->string(‘ContentType’);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items');
+    }
+}
