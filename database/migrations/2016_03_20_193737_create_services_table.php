268e2b6345ef00a9b34f7a62f55b7a7f9c223b8c
alisonmurat <amurat@gmu.edu>
2016-04-10 12:33:34 -0400

    change

--- database/migrations/2016_03_20_193737_create_services_table.php
+++ database/migrations/2016_03_20_193737_create_services_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateServicesTable extends Migration
+class CreateServiceTable extends Migration
 {
     /**
      * Run the migrations.
