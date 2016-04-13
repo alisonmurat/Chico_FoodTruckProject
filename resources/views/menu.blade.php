7184793bce2678dee124a266f18bdc115cf6eca2
rchoudh7 <rchoudh7@masonlive.gmu.edu>
2016-04-12 22:07:41 -0400

    fixed menu url

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background-color: rgb(185, 0, 0);}
+ul#menu {
+    padding: 2;
+}
+
+ul#menu li {
+    display: inline;
+}
+
+ul#menu li a {
+    background-color: rgb(53, 124, 49);
+    color: white;
+    padding: 10px 60px;
+    text-decoration: none;
+    border-radius: 4px 4px 0 0;
+}
+
+ul#menu li a:hover {
+    background-color: lightgreen;
+}
+img { 
+    width:100%; 
+</style>
+</head>
+<body>
+
+
+
+<ul id="menu">
+<center>
+  <li><a href="/aboutus">About Us</a></li>
+  <li><a href="/">Home Page</a></li>
+  <li><a href="/home">Menu</a></li>
+  <li><a href="/gallery">Gallery</a></li>
+  <li><a href="/location">Location & Hours</a></li>
+</center>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>	
+</ul>  
+<center>
+
+<img src="/photos/1.jpg" alt="Team" style="width:460px;height:260px;"> <center>$4.99 Classic</center>
+<br>
+<br>
+<img src="/photos/2.jpg" alt="Truck" style="width:460px;height:260px;"> <center>$6.99 Taco Delight</center>
+<br>
+<br>
+<img src="/photos/3.jpg" alt="Truck" style="width:460px;height:260px;"> <center>$9.99 Taco Tacos</center>
+<br>
+<br>
+<img src="/photos/13.jpg" alt="Truck" style="width:460px;height:260px;"> <center>$19.99 Classic II</center>
+<br>
+<br>
+<br>
+
+</center>
+</body>
+</html>
