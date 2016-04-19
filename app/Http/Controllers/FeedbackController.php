e93c7f36c182a39f4e9b989b069a0b502abcfe6e
xnativi05 <xnativi@masonlive.gmu.edu>
2016-04-18 22:57:32 -0400

    FeedbackController change don't know if it's right

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -10,7 +10,7 @@ use Log;
 class FeedbackController extends Controller
 {
 	/**
-     * Show a list of all of the application's Items.
+     * Show a list of all of the application's feedback.
      *
      * @return Response
      */
@@ -21,5 +21,18 @@ class FeedbackController extends Controller
          $feedback = DB::table('feedback')->get();
          return view('feedback', ['feedback' => $feedback]);
 
+    }
+    public function save_feedback_form(Request $request)
+    {
+        Log::debug($request->all());
+
+        $data = $request->all();
+        $success = Feedback::create($data);
+        
+        // $success = User::create(
+        //     ['email' => $data['email']]
+        // );
+        $feedback = Feedback::where('feedback_label', 'thanks')->first();
+        return view('feedback', $feedback);
     }
 }
