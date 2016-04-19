fcf7f07e23338d1a64d1c4067f7944b31801840f
rchoudh7 <rchoudh7@masonlive.gmu.edu>
2016-04-18 21:25:36 -0400

    location page

---
+++ resources/views/location.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<ul id="menu">
+<center>
+  <li><a href="/aboutus">About Us</a></li>
+  <li><a href="/">Home Page</a></li>
+  <li><a href="/home">Menu</a></li>
+  <li><a href="/gallery">Gallery</a></li>
+  <li><a href="/location">Location & Hours</a></li>
+</center>  
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+</ul> 
+
+
+<style>
+body {
+	background-image: url("http://www.ndm.ox.ac.uk/mexico/_asset/image/mexican-blanket-projects-icon.jpeg")
+}
+ul#menu {
+    padding: 2;
+}
+
+ul#menu li {
+    display: inline;
+}
+.boxed {
+  border: 3px solid green ;
+}
+
+ul#menu li a {
+    background-color: rgb(53, 124, 49);
+    color: white;
+    padding: 10px 60px;
+    text-decoration: none;
+    border-radius: 4px 4px 0 0;
+}
+
+ul#menu li a:hover {
+    background-color: lightgreen;
+}
+img { 
+    width:100%; 
+</style>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<font font size="3" 
+color="black">
+<div class="boxed">
+<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:400px;width:700px;'><div id='gmap_canvas' style='height:400px;width:700px;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="http://www.autohuren.world/">auto huren</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(38.905239,-77.06864239999999),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(38.905239,-77.06864239999999)});infowindow = new google.maps.InfoWindow({content:'<strong>Title</strong><br>3423 M St NW, Washington, DC 20007<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+</div>
+</head>
+<body>
+
+<br>
+<p>
+<center>
+<br>
+<br>
+<br>
+<br>
+<font font size="3" 
+color="yellow">
+<div class="boxed">
+  Location<br>3423 M St NW, Washington, DC 20007<br>
+</div>
+</font>
+<br>
+<br>
+<br>
+<font font size="3" 
+color="yellow">
+<div class="boxed">
+  Hours of Operations<br>Monday - Thursday (9am - 8pm). Friday & Saturday (12pm - 12am). Sunday (12pm - 6pm).<br>
+</div>
+</font>
+<br>
+<br>
+<br>
+<font font size="3" 
+color="yellow">Come Checkout our authentic mexican food at our Food Truck located in Washington D.C metro area.</font>
+</center>
+</p
+
+</body>
+</html>
