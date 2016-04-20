bc8eef733bef56343c3aa805aba56f873f5941b7
rchoudh7 <rchoudh7@masonlive.gmu.edu>
2016-04-19 20:00:43 -0400

    location page

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -57,9 +57,7 @@ img {
 <br>
 <font font size="3" 
 color="black">
-<div class="boxed">
 <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:400px;width:700px;'><div id='gmap_canvas' style='height:400px;width:700px;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="http://www.autohuren.world/">auto huren</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(38.905239,-77.06864239999999),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(38.905239,-77.06864239999999)});infowindow = new google.maps.InfoWindow({content:'<strong>Title</strong><br>3423 M St NW, Washington, DC 20007<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
-</div>
 </head>
 <body>
 
