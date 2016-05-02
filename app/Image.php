341b45197362503c2956ff2877b3a6a0b9af4401
alisonmurat <amurat@gmu.edu>
2016-05-02 15:40:36 -0400

    Model:Image corrected

--- app/Image.php
+++ app/Image.php
@@ -1,11 +1,13 @@
 +<?php
-+
-+namespace App;
-+
-+use Illuminate\Database\Eloquent\Model;
-+
-+class Image extends Model
-+{
-+    //
-+    protected $guarded = ['id', 'created_at', 'updated_at'];
-+}
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    //
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+    protected $table = "Image";
+
+}
