ed1b748716952c47ab0bb319aa69b416ae988078
rchoudh7 <rchoudh7@masonlive.gmu.edu>
2016-05-03 13:27:21 -0400

    more loops

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -57,7 +57,7 @@ img {
   <li><a href="/">Home Page</a></li>
   <li><a href="/menu">Menu</a></li>
   <li><a href="/gallery">Gallery</a></li>
-  <li><a href="location">Location & Hours</a></li>
+  <li><a href="/location">Location & Hours</a></li>
 </center>
   <br>
   <br>
@@ -66,23 +66,24 @@ img {
   <br>
 </ul>  
 <center>
-@foreach( $gallery->images as $gallery)
-<a href="/photos/{{$g->id}}">{{ $g->name }}</a> &nbsp;&nbsp;&nbsp;
+@foreach( $gallery as $g)
+<a href="/gallery{{$g->1}}">{{ $g->name }}</a>
+<img src="/gallery{{$g->filename}}">
 <div class="cfg">
-<img id="thumbnailImage1" src="/photos/1.jpg" alt="food" style="width:160px;height:160px;"> 
-<img id="thumbnailImage2" src="/photos/2.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage3" src="/photos/3.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage4" src="/photos/4.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/1.jpg" alt="food" style="width:160px;height:160px;"> 
+<img src="/photos/2.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/3.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/4.jpg" alt="Truck" style="width:160px;height:160px;">
 <br>
-<img id="thumbnailImage5" src="/photos/5.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage6" src="/photos/6.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage7" src="/photos/7.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage8" src="/photos/8.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/5.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/6.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/7.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/8.jpg" alt="Truck" style="width:160px;height:160px;">
 <br>
-<img id="thumbnailImage9" src="/photos/9.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage10" src="/photos/10.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage11" src="/photos/11.jpg" alt="Truck" style="width:160px;height:160px;">
-<img id="thumbnailImage12" src="/photos/12.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/9.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/10.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/11.jpg" alt="Truck" style="width:160px;height:160px;">
+<img src="/photos/12.jpg" alt="Truck" style="width:160px;height:160px;">
 </div>
 @endforeach
 </center>
